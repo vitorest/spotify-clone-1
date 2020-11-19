b3dfc5621ffc0f01fbcbf3d644d92bb55260e9de
Vítor Castro <64875903+vitorest@users.noreply.github.com>
2020-11-19 08:52:13 +0000

    spotify clone

---
+++ src/Footer.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import './Footer.css';
+import PlayCircleOutlineIcon from "@material-ui/icons/PlayCircleOutline";
+import SkipPreviousIcon from "@material-ui/icons/SkipPrevious";
+import SkipNextIcon from "@material-ui/icons/SkipNext";
+import ShuffleIcon from "@material-ui/icons/Shuffle";
+import VolumeDownIcon from "@material-ui/icons/VolumeDown";
+import PlaylistPlayIcon from "@material-ui/icons/PlaylistPlay";
+import "./Footer.css";
+import RepeatIcon from "@material-ui/icons/Repeat";
+import PauseCircleOutlineIcon from "@material-ui/icons/PauseCircleOutline";
+import { Grid, Slider } from "@material-ui/core";
+
+function Footer() {
+  return (
+    <div className='footer'>
+      <div className='footer__left'>
+        <img
+          className='footer__albumLogo'
+          src="https://.wikipedia.org/wiki/Ficheiro:Usher_Raymond_by_Sandra_Alphonse.jpg"
+          alt=""
+        />
+        <div className='footer__songInfo'>
+          <h4>Yeahhh</h4>
+          <p>Usher</p>
+        </div>
+      </div>
+      <div className='footer__center'>
+        <ShuffleIcon className='footer__green' />
+        <SkipPreviousIcon className='footer__icon' />
+        <PlayCircleOutlineIcon className='footer__icon' fontSize='large' />
+        <SkipNextIcon className='footer__icon' />
+        <RepeatIcon className='footer__green' />
+      </div>
+      <div className='footer__right'>
+        <Grid container spacing={2}>
+          <Grid item>
+            <PlaylistPlayIcon />
+          </Grid>
+          <Grid item>
+            <VolumeDownIcon />
+          </Grid>
+          <Grid item xs>
+            <Slider />
+          </Grid>
+        </Grid>
+      </div>
+    </div>
+
+
+  )
+}
+export default Footer;
