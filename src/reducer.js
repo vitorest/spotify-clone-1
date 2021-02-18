e915f1bb4beee62e666ade96363d40da89076ad2
Vítor Castro <64875903+vitorest@users.noreply.github.com>
2021-02-18 16:52:52 +0000

    Update reducer.js

--- src/reducer.js
+++ src/reducer.js
@@ -3,8 +3,6 @@ export const initialState = {
   playlists: [],
   playing: false,
   item: null,
-  //REMOVE after finished developing...
-  //token: 'BQDi5j_0Ex8JQQ08oRSPOKW3UVh2LHKg0XFF3hcmPDY4RS38se2JCKG1sX3jRz8qeCsOX1aNwZljbbzzDfDEwmnlcxb6DX3COUFWZtmn7CxHQBXYWDh73_xGlSXEgMUXaAPVf1cjoS5FRZkjz9KlYmgNgJnv'
 }
 
 const reducer = (state, action) => {
